370d5d62348f334a477ec4f1cb0f655127f74043
Alexander <qugluschenko@gmail.com>
2015-05-15 13:38:02 +0300

    Update Physics.cs

--- src/ElectricalKit/Physics.cs
+++ src/ElectricalKit/Physics.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * This is the source code of ElectricalKit.
+ * It is licensed under MIT License.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Alexander Gluschenko, 2014-2015.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
